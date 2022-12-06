25c76234b56f7f8d2b7b8ae270c4806ea12e8f8e
Nick <Echophase44@github.com>
2022-12-05 22:30:04 -0500

    Updated login page with layout, icons and images

--- Bread-app/src/App.jsx
+++ Bread-app/src/App.jsx
@@ -1,10 +1,45 @@
 import { useState } from 'react'
+import bread from "../public/login-bread.jpg"
+import logo from "../public/breadLogo.png"
 
 
 function App() {
  return (
- <div>
-  <h1>Test</h1>
+ <div className='login-container'>
+  <img className='login-img' src={bread} alt="Bread" />
+  <div className='login-contentContainer'>
+    <div className='login-text'>
+      <p>Start with a simple receipe and record the changes as you go.</p>
+      <img className='login-logo' src={logo} alt="Logo" />
+    </div>
+    
+    <div className='login-formContainer'>
+      <p className='login-formHeader'>Bake. Eat. Revise.</p>
+      <form id="login-form" action="">
+
+        <input 
+        type="text"
+        placeholder='Username' 
+        />
+        
+        <input 
+        type="text"
+        placeholder='Password' 
+        name="" 
+        id="" />
+        
+        <input 
+        type="text"
+        placeholder='Confirm Password' 
+        name="" 
+        id="" />
+      </form>
+    </div>
+    <div className='login-bottomContent'>
+      <button className='login-button'>Create Account</button>
+      <p>Already have an account? <span>Login</span></p>
+    </div>
+  </div>
  </div>
  )
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Bread_app
+
+## Credits
+
+<a href="https://www.flaticon.com/free-icons/bread" title="bread icons">Bread icons created by Payungkead - Flaticon</a>
